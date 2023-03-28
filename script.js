2baa5713140705723d13f67dbe7fbd01bda05460
slady <csl.bunny.2207@gmail.com>
2023-03-28 23:08:48 +0200

    passing arguments to event handlers - nav links

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,12 +39,12 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+//////////////////////////////////////
+/// Scrolling
 
 btnScrollTo.addEventListener('click', function (e) {
   // Get the coordinates of the el that we want ot scroll to
-  const s1coords = section1.getBoundingClientRect(); // this method is relative to visible viewport (x and y properties change values as we scroll and change position of the elment on viewport basically)
+  const s1coords = section1.getBoundingClientRect(); // this method is relative to visible viewport (x and y properties change values as we scroll and change position of the element on viewport basically)
 
   // console.log(s1coords); //  DOMrect with positions, x and y positions, width and height of the element....
   // console.log(e.target.getBoundingClientRect()); // e.target is element that was clicked (here it's btnScrollTo)
@@ -65,7 +74,8 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-// Page navigation
+//////////////////////////////////////
+//// Page navigation
 
 // document.querySelectorAll('.nav__link').forEach(function (el) {
 //   el.addEventListener('click', function (e) {
@@ -85,20 +95,16 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
   // Matching strategy (to find only links that we are interested in, bc we want event only when we click on certain parts, not the entire container of nav bar)
   if (e.target.classList.contains('nav__link')) {
-    console.log('LINK');
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
+///////////////////////////////////////////
 // Tabbed component
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.operations__tab');
+  const clicked = e.target.closest('.operations__tab'); // there are span elements on the button that we can accidentally click, so we need to use closest() to find right element, to find a perent that matches a certain query
 
   // Guard clause
   if (!clicked) return; // if there's nothing clicked then we want to immediately finish this func
@@ -115,3 +121,38 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+////////////////////////////////
+// Menu fade animation
+
+// (mouseenter doesn't bubble, mouseover bubbles up)
+
+const handleHover = function (e, opacity) {
+  // there are no chinld elements that we could accidentally click when we click on links, so we don't need closest():
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
+// nav.addEventListener('mouseout', function (e) {
+//   // opposite of mouseover, to undo things that happen with mouseover
+//   handleHover(e, 1);
+// });
+
+// better way - using bind() that creates a copy of the function that is called on and it will set this kw in this funcion call to whatever value that we pass into bind
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// it's impossible to pass another argument into and eventHandler function, it only has 1 parameter which is event. so if we want to pass additional values into the handler function that we need to use this kw, that becomes possible with using bind()
